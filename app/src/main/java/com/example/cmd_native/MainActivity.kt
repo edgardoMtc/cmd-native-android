f51c2f0d2e4b35923aab6b9f2a8bff0b939969c3
eperez <edgardo@mediatradecraft.com>
2025-04-07 15:48:19 -0400

    added code

--- app/src/main/java/com/example/cmd_native/MainActivity.kt
+++ app/src/main/java/com/example/cmd_native/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private RadioButton rdFromKm
+    private RadioButton rdFromMiles
+    private EditText textValue
+    private EditText textResult
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rdFromKm = findViewById<Boolean>(R.id.rbKm)
     }
 }
