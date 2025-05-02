4faf7864a9c3e02b25bdc29b045f63077ec1be91
eperez <edgardo@mediatradecraft.com>
2025-05-02 12:43:14 -0400

    added configs for google ad manager in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-ads:24.2.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/example/cmd_native/MainActivity.kt
+++ app/src/main/java/com/example/cmd_native/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.cmd_native
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.RadioButton
 import android.view.View
+import android.widget.Toast
+
 // import androidx.core.text.set
 
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rdFromMiles: RadioButton
     private lateinit var textValue: EditText
     private lateinit var textResult: EditText
+    private lateinit var appContext: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +31,24 @@ class MainActivity : AppCompatActivity() {
     fun handleClick(view: View) {
         when (view.id) {
             (R.id.buttonConvert) -> {
-               // var result: String? = null
                val value = textValue.text.toString()
-                val result: String = if (rdFromKm.isChecked) {
-                    kmToMiles(value)
+                if (value.isEmpty()) {
+                    val context = appContext.applicationContext
+                    val errorText = "Error: Empty value!"
+                    val duration = Toast.LENGTH_SHORT
+                    val toast = Toast.makeText(context, errorText, duration)
+                    toast.show()
+
                 } else {
-                    milesToKM(value)
+                    val result: String = if (rdFromKm.isChecked) {
+                        kmToMiles(value)
+                    } else {
+                        milesToKM(value)
+                    }
+                    textResult.setText(result)
                 }
-                textResult.setText(result)
             }
+
             (R.id.buttonReset) -> {
                 rdFromKm.isChecked = false
                 rdFromMiles.isChecked = false
